fd18f251891f312098f85f7d1156241d954889f0
Onome <onome.sotu@me.com>
2018-01-18 22:48:56 +0200

    Tested gulpfile to track error report

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('css', function(){
-	gulp.src('./css/*.css')
-		.pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false })).
-			
+/*gulp.task('css', function(){
+	gulp.src('./css/main.css')
+		.pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false }))
+		.pipe(gulp.dest('./css/style.css'))	
 		.pipe(browerSync.reload({
 			stream: true
 		}));
-});
+});*/
 
 gulp.task('browerSync', function(){
 	browserSync.init({
@@ -19,8 +19,8 @@ gulp.task('browerSync', function(){
 	});
 });
 
-gulp.task('start', ['browerSync', 'css'], function(){
-	gulp.watch('./css/*.css', ['css']);
+gulp.task('start', ['browerSync'], function(){
+	gulp.watch('./css/*.css', browerSync.reload);
 	gulp.watch('index.html', browserSync.reload);
 	gulp.watch('./scripts/*.js', browserSync.reload);
 });
