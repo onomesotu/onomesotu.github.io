247b8c40b6101e400a6865fd23a47a0cd75f7479
Onome <onome.sotu@me.com>
2018-01-20 21:47:42 +0200

    Fixed gulp error emanating from a syntax error and wrongly used variable name

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 
-/*gulp.task('css', function(){
-	gulp.src('./css/main.css')
+gulp.task('css', function(){
+	return gulp.src('./css/main.css')
 		.pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false }))
-		.pipe(gulp.dest('./css/style.css'))	
-		.pipe(browerSync.reload({
+		.pipe(gulp.dest('./css'))	
+		.pipe(browserSync.reload({
 			stream: true
 		}));
-});*/
+});
 
-gulp.task('browerSync', function(){
+gulp.task('browserSync', function(){
 	browserSync.init({
 		server: {
 			baseDir: './'
@@ -19,8 +19,8 @@ gulp.task('browerSync', function(){
 	});
 });
 
-gulp.task('start', ['browerSync'], function(){
-	gulp.watch('./css/*.css', browerSync.reload);
+gulp.task('start', ['browserSync', 'css'], function(){
+	gulp.watch('./css/*.css', ['css']);
 	gulp.watch('index.html', browserSync.reload);
 	gulp.watch('./scripts/*.js', browserSync.reload);
 });
